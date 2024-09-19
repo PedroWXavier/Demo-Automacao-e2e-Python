f45c938100a95d456792949a74b9efe606d0b108
pedro.xavier <pedro.xavier@e-unicred.com.br>
2024-09-18 22:00:52 -0300

    .

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from behave import __main__ as behave_executable
 
-from resources.commons.driver import Driver
-
 debug = False
 
 if __name__ == "__main__":
     behave_executable.main('features/login.feature --no-capture --no-capture-stderr')
+    
